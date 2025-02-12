3823ac6d28da0d0673013478a32e29fdc33e6c41
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-12 11:23:28 +0900

    v0.1 fork

--- day03.py
+++ day03.py
@@ -28,12 +28,32 @@ def my_pow(b, e) -> float:
     return result
 
 
-print(my_pow(10, -2))
-print(my_pow(2, 9))
-print(my_pow(16, 0.5))
-print(my_pow(10, 3))
-print(my_pow(25, 0.5))  # ieee 754
+def is_prime(num) -> bool:
+    if num >= 2:
+        i = 2
+        while i <= my_pow(num, 0.5):
+            if num % i == 0:
+                return False
+            i = i + 1
+    else:
+        return False
+    return True
 
-# print(math.exp(1))
-# print(math.e)
-# print(math.log(16, 2))
+
+number = input("First number Second number : ").split()
+
+n1 = int(number[0])
+n2 = int(number[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+cnt = 0
+while n1 <= n2:
+    if is_prime(n1):
+        print(f"{n1} ", end='')
+        cnt = cnt + 1
+    if cnt == 20:
+        cnt = 0
+        print()
+    n1 = n1 + 1
